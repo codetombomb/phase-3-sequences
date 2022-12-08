6613cc07558e7e490ba71db15ef0866af50dd5d8
Tom Tobar <cocdetombomb@gmail.com>
2022-12-08 14:00:34 -0600

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0,1])
+    else:
+        sequence = [0, 1]
+        count = 0
+        while len(sequence) < length:
+            sequence.append(sequence[-1] + sequence[-2])
+            count += 1
+        print(sequence)
+        
+
+print(print_fibonacci(10))
